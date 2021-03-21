6f339e1a86cc81a6e30eda3b9a63a8726580f0e3
grupoDAI20202021 <grupoDAI20202021@gmail.com>
2021-03-21 23:07:25 +0000

    jose barros 21/03/2021 23:07

--- src/main/java/daibackend/demo/model/Preference.java
+++ src/main/java/daibackend/demo/model/Preference.java
@@ -1,4 +1,52 @@
 package daibackend.demo.model;
 
+import javax.persistence.*;
+
+@Entity(name = "Preference")
+@Table(name = "Preference")
 public class Preference {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_preference;
+
+    @ManyToOne
+    @JoinColumn(name = "id_child", referencedColumnName = "id_child", nullable = false)
+    private Child child;
+
+    @ManyToOne
+    @JoinColumn(name = "id_activity_type", referencedColumnName = "id_activity_type", nullable = false)
+    private ActivityType activityType;
+
+    public Preference() {
+    }
+
+    public Preference(Long id_preference, Child child, ActivityType activityType) {
+        this.id_preference = id_preference;
+        this.child = child;
+        this.activityType = activityType;
+    }
+
+    public Long getId_preference() {
+        return id_preference;
+    }
+
+    public void setId_preference(Long id_preference) {
+        this.id_preference = id_preference;
+    }
+
+    public Child getChild() {
+        return child;
+    }
+
+    public void setChild(Child child) {
+        this.child = child;
+    }
+
+    public ActivityType getActivityType() {
+        return activityType;
+    }
+
+    public void setActivityType(ActivityType activityType) {
+        this.activityType = activityType;
+    }
 }
--- src/main/java/daibackend/demo/model/Sugestion.java
+++ src/main/java/daibackend/demo/model/Sugestion.java
@@ -1,4 +1,82 @@
 package daibackend.demo.model;
 
+
+import daibackend.demo.util.ConstantUtils;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Entity(name = "Sugestion")
+@Table(name = "Sugestion")
 public class Sugestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_sugestion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_child", referencedColumnName = "id_child", nullable = false)
+    private Child child;
+
+    @NotBlank(message = "Can't be blank")
+    @Pattern(regexp = ConstantUtils.CHAR_PATTERN, message = "Can only letters, letters with special characters and spaces")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "id_image", referencedColumnName = "id_image", nullable = false)
+    private ImageExample imageExample;
+
+    private byte voice;
+
+    public Sugestion() {
+    }
+
+    public Sugestion(Long id_sugestion, Child child,  String content, ImageExample imageExample, byte voice) {
+        this.id_sugestion = id_sugestion;
+        this.child = child;
+        this.content = content;
+        this.imageExample = imageExample;
+        this.voice = voice;
+    }
+
+    public Long getId_sugestion() {
+        return id_sugestion;
+    }
+
+    public void setId_sugestion(Long id_sugestion) {
+        this.id_sugestion = id_sugestion;
+    }
+
+    public Child getChild() {
+        return child;
+    }
+
+    public void setChild(Child child) {
+        this.child = child;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public ImageExample getImageExample() {
+        return imageExample;
+    }
+
+    public void setImageExample(ImageExample imageExample) {
+        this.imageExample = imageExample;
+    }
+
+    public byte getVoice() {
+        return voice;
+    }
+
+    public void setVoice(byte voice) {
+        this.voice = voice;
+    }
 }
